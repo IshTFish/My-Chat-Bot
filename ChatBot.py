5d4cdaac850286be3252c1759d1a95a62ad30d04
IshTFish <88210053+IshTFish@users.noreply.github.com>
2023-07-24 14:42:58 -0700

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -54,5 +54,10 @@ def call_langchain(prompt: str) -> str:
     return output
 
 question = st.text_input('Enter a question here', '')
-response = call_chatgpt(question)
+ref_from_internet = call_langchain(question)
+engineered_prompt = f""""
+    Basedon the context: {ref_from_internet}, 
+    answer the user question {question}
+""""
+response = call_chatgpt(engineered_prompt)
 st.write(response)
